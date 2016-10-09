57316b3973673814f8186461d3269c1f132a805e
izuoka <maiueox@gmail.com>
2016-10-10 01:44:33 +0900

    tabクラスの追加に伴い、描画の仕方を修正

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ ipc.on('asynchronous-message', function(event, arg) {
     var message = arg[0];
     if (message == 'Draw') {
         var ops = arg[1].ops;
-        jquery(".tab").html("");
+        var tab = jquery(".tab");
+        var l_window = tab.find(".labels-window");
+        var p_window = tab.find(".pipelines-window");
+        l_window.empty(); p_window.empty();
         for (var i = 0; i < ops.length; i++) {
             op = new OpToHTML(ops[i]);
-            //console.log(op.html());
             var label = op.node().find(".labels-parent");
             var pipeline = op.node().find(".pipeline");
-            jquery(".labels-window").append(label);
-            jquery(".pipelines-window").append(pipeline);
-            var lineHeight = pipeline.css("max-height");
-            lineHeight = 48;
-            console.log(lineHeight);
-            jquery(".labels-parent").css("height", lineHeight + "px");
-            jquery(".pipeline").css("height", lineHeight + "px");
-            //jquery(".pipeline").css("position", "relative");
+            var lineHeight = 24;//pipeline.css("max-height");
+
+            label.css("height", lineHeight + "px");
+            pipeline.css("height", lineHeight + "px");
+            l_window.append(label);
+            p_window.append(pipeline);
         }
     }
 });
