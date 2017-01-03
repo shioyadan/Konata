6fd98b0c86244bdd3e7502b4db76960600f8f4c7
Ryota Shioya <shioya@nuee.nagoya-u.ac.jp>
2017-01-04 01:44:26 +0900

    eslint の設定を追加

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "jquery": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+
+        // var は不許可に
+        "no-var": [
+            "error"
+        ],
+
+        // riot タグ間で二重定義とされてしまうので無効化
+        // let の場合，二重定義は実行時にエラーとされる
+        "no-redeclare": [
+            "off"
+        ]
+    },
+
+    // HTML 内のスクリプトをチェックするためにプラグインを有効化
+    "plugins": ["html"],
+
+    // let/const/forOf などを使う
+    "parserOptions": {
+        "ecmaVersion": 6,
+    },
+};
