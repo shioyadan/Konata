8491ef1cdf284b4dd9e0ca170542b2d7ba630f9a
Ryota Shioya <shioya@nuee.nagoya-u.ac.jp>
2017-01-05 21:55:05 +0900

    タブの表示部分を作成

--- store.js
+++ store.js
@@ -7,7 +7,7 @@ const ACTION = {
     FILE_OPEN: 10,
     FILE_CLOSE: 11,
 
-    TAB_OPEN: 20,
+    TAB_UPDATE: 20,
     TAB_CLOSE: 21,
 
     APP_QUIT: 30
@@ -21,12 +21,26 @@ function Store(){
 
     let self = this;
     self.fileName = null;
-        
+
+    // Tab
+    self.tabs = [];
+    self.nextTabID = 0;
+    self.activeTabID = 0;
 
     // ファイルオープン
     self.on(ACTION.FILE_OPEN, function(fileName){
         self.fileName = fileName;
-        self.trigger(ACTION.TAB_OPEN, fileName);
+
+        // Create a new tab
+        let tab = {
+            id: self.nextTabID, 
+            fileName: fileName
+        };
+        self.tabs.push(tab);
+        self.activeTabID = self.nextTabID;
+        self.nextTabID++;
+        
+        self.trigger(ACTION.TAB_UPDATE, self, tab);
     });
 
     // ファイルクローズ
