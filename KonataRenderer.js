6ac7d68b9770fcfb336842b43e3329d6347b6ca9
Ryota Shioya <shioya@nuee.nagoya-u.ac.jp>
2017-01-07 22:45:47 +0900

    不用なコードを削除

--- KonataRenderer.js
+++ KonataRenderer.js
@@ -9,31 +9,17 @@ function KonataRenderer(){
     let m_scale = null;
     this.konata = null;
 
-    // jQuery HTMLをいじるときに使う．
-    let m_jquery = require("./lib/js/jquery");
-
-    // キャンバスの縦横．0でなければなんでもいいと思う．
-    let m_canvasW = 300;
-    let m_canvasH = 300;
-
     // 以下のパラメータはOp.jsと合わせる．(そうしないと表示がズレる)
     let m_opH = 25; // スケール1のときの1命令の高さ
     let m_opW = 25; // スケール1のときの1サイクルの幅
     let m_skip = 1;
-    let m_retina = false;//retina;
 
-    let m_normalScale = m_retina? 2:1;
-    let m_maxScale = m_retina? 4:2; // retinaの場合、倍精度必要なので最大倍率も倍
-    let m_minScale = m_retina? 0.00006103515625 * 2: 0.00006103515625;
+    let m_normalScale = 1;
+    let m_maxScale = 2; // retinaの場合、倍精度必要なので最大倍率も倍
+    let m_minScale = 0.00006103515625;
     
     this.GetScale = function(){
         return m_scale;
-    };
-
-    this.RetinaSwitch = function () {
-        m_retina = !m_retina;
-        m_maxScale = m_retina? 4:2;
-        m_minScale = m_retina? 0.00006103515625 * 2: 0.00006103515625;
     };
 
     this.ParentStyle = function(style, value){
@@ -54,7 +40,6 @@ function KonataRenderer(){
 
     // Use renderer process only
     this.draw = function(canvas){
-        //this.SetTile();
         let pos = m_position;
         let scale = m_scale;
         let top = pos.top;
@@ -62,19 +47,6 @@ function KonataRenderer(){
 
         m_skip = Math.floor(20/(scale * Math.log(scale)/0.005));
         this.DrawTile(canvas, top, left);
-
-        /*
-        for (let y = 0; y < tiles.length; y++) {
-            let left = pos.left;
-            for (let x = 0; x < tiles[y].length; x++) {
-                let tile = tiles[y][x];
-                this.DrawTile(tile, top, left);
-                left += m_canvasW/(scale * m_opW);
-            }
-            top += m_canvasH/(scale * m_opH);
-        }
-        //SetPrefetch(this);
-        */
         return true;
     };
 
@@ -103,21 +75,15 @@ function KonataRenderer(){
                 m_position.left = 0;
             }
         }
-        this.Draw();
     };
 
     this.Zoom = function (scale) {
-        if (m_tiles == null) {
-            console.log("tile null");
-            return;
-        }
         m_scale = m_scale * scale;
         if (m_scale > m_maxScale) {
             m_scale = m_maxScale;
         } else if (m_scale < m_minScale) {
             m_scale = m_minScale;
         }
-        this.Draw();
     };
 
     // private methods
@@ -144,52 +110,6 @@ function KonataRenderer(){
             }
         }
     };
-
-    this.SetTile = function(){
-        // canvasのサイズを定義する[px]
-        let tab = m_tabs;
-        let p = tab.find(".pipelines-window");
-        // 必要なcanvas数を考える
-        let x, y;
-        if (m_retina) { // retinaだと倍精度で描かないとボケる
-            x = Math.ceil(p.width()/m_canvasW) * 2 + 2;
-            y = Math.ceil(p.height()/m_canvasH) * 2 + 2;
-        } else {
-            x = Math.ceil(p.width()/m_canvasW) + 2;
-            y = Math.ceil(p.height()/m_canvasH) + 2;
-        }
-        LayTiles(p, x, y);
-        //console.log(key , "set tiles:", p.width(), p.height());
-    };
-
-    // obj内に幅width, 高さheightのタイルをx * y個敷き詰める。
-    function LayTiles(obj, x, y) {
-        obj.html("");
-        let tiles = [];
-        for (let h = 0; h < y; h++) {
-            let tileY = m_jquery("<div></div>", {class:"tileY"}).appendTo(obj);
-            if (m_retina) {
-                tileY.css("max-height", m_canvasH/2);
-            } else {
-                tileY.css("max-height", m_canvasH);
-            }
-            tiles.push([]);
-            for (let w = 0; w < x; w++) {
-                let tileX = m_jquery("<canvas></canvas>", {class:"tileX"}).appendTo(tileY);
-                tileX.attr("width", m_canvasW);
-                tileX.attr("height", m_canvasH);
-                if (m_retina) {
-                    tileX.css({"width":m_canvasW/2, "height":m_canvasH/2}); // Retinaディスプレイの解像度に対応
-                }
-                if (!tileX[0].getContext) {
-                    console.log("tileX.getContext not found");
-                    return;
-                }
-                tiles[h].push( tileX[0].getContext("2d") );
-            }
-        }
-        m_tiles = tiles;
-    }
 }
 
 module.exports = KonataRenderer;
