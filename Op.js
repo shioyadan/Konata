2e3fcb880a9875d62b84dfb18665346e71a70140
Ryota Shioya <shioya@nuee.nagoya-u.ac.jp>
2017-02-05 20:06:01 +0900

    Op で class 構文を使うように

--- Op.js
+++ Op.js
@@ -1,23 +1,23 @@
-function Op(args){
-    this.id = null;
-    this.gid = null; // シミュレータ上のグローバルID
-    this.rid = null; // シミュレータ上のリタイアID
-    this.tid = null; // スレッドID
-    this.retired = false; // リタイアしてるかどうか
-    this.flush = false; // Flushであるかどうか
-    this.eof = false; // ファイル終端による終了
-    this.lanes = {}; // レーン情報の連想配列
-    this.fetchedCycle = null;
-    this.retiredCycle = null;
-    this.labels = []; // ラベル情報の入っている配列
-    this.prods = []; // プロデューサ命令のIDの配列
-    this.cons = []; // コンシューマ命令のIDの配列
+class Op{
+    constructor(args){
+        this.id = null;
+        this.gid = null; // シミュレータ上のグローバルID
+        this.rid = null; // シミュレータ上のリタイアID
+        this.tid = null; // スレッドID
+        this.retired = false; // リタイアしてるかどうか
+        this.flush = false; // Flushであるかどうか
+        this.eof = false; // ファイル終端による終了
+        this.lanes = {}; // レーン情報の連想配列
+        this.fetchedCycle = null;
+        this.retiredCycle = null;
+        this.labels = []; // ラベル情報の入っている配列
+        this.prods = []; // プロデューサ命令のIDの配列
+        this.cons = []; // コンシューマ命令のIDの配列
 
-
-    for (let key in args) {
-        this[key] = args[key];
+        for (let key in args) {
+            this[key] = args[key];
+        }
     }
 }
-
 
 module.exports.Op = Op;
