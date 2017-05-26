a0eca136ac547ffe1ef937e78417e9fdb5012062
Ryota Shioya <shioya@nuee.nagoya-u.ac.jp>
2017-05-26 23:14:12 +0900

    バージョン番号を更新

--- menu.js
+++ menu.js
@@ -152,7 +152,7 @@ function installMainMenu(){
                         click: function(){
                             rc.trigger(
                                 ACTION.DIALOG_MODAL_MESSAGE,
-                                "Konata ver 0.11, Kojiro Izuoka and Ryota Shioya."
+                                "Konata ver 0.12, Kojiro Izuoka and Ryota Shioya."
                             );
                         }
                     }
