883d3780ded60d56463fe97390f3afe484ce49d0
Ryota Shioya <shioya@nuee.nagoya-u.ac.jp>
2017-07-16 23:34:29 +0900

    konata モジュールのコードを整理

--- konata.js
+++ konata.js
@@ -1,21 +1,12 @@
 class Konata{
     constructor(){
         this.name = "Konata";
-
-        // private変数．外部からはアクセサを用意しない限りアクセスできない．
-        // ローカル変数と区別するため this. を付ける．
-        this.cache_ = null; // 見たいファイル名とパース結果を関連付ける連想配列
-        this.lastFetchedId = 0;
         this.parser_ = null;
-
         this.FileReader_ = require("./file_reader").FileReader;
         this.OnikiriParser_ = require("./onikiri_parser").OnikiriParser;
-        this.OpCache_ = require("./op_cache").OpCache;
     }
 
     close(){
-        this.cache_ = null;
-        this.lastFetchedId = null;
         this.parser_ = null;
     }
 
@@ -32,12 +23,10 @@ class Konata{
 
         parser.setFile(file);
         console.log("Selected parser:" , parser.getName());
-        this.cache_ = new this.OpCache_(path, parser);
     }
 
     getOp(id){
-        return this.cache_.getOp(id);
-        //return this.parser_.getOp(id);
+        return this.parser_.getOp(id);
     }
 
     getOpFromRID(rid){
@@ -56,49 +45,6 @@ class Konata{
         return this.parser_.stageLevelMap;
     }
 
-    /*
-    // プリフェッチも一旦無効に
-
-    this.prefetch = null;
-    this.prefetchInterval = 1000;
-    this.prefetchNum = 1000;
-
-    // なにか時間のかかりそうな処理の前には呼び出す．
-    this.CancelPrefetch = function(){
-        if (this.prefetch !== null) {
-            clearTimeout(this.prefetch);
-            this.prefetch = null;
-        }
-    };
-
-    // 時間のかかりそうな処理の終わりに呼んでおく．
-    function SetPrefetch (self) {
-        this.CancelPrefetch(); // 多重予約を防ぐために予約済のプリフェッチがあればキャンセルする．
-        this.prefetch = setTimeout(function(){Prefetch(self);}, this.prefetchInterval);
-    }
-
-    // この関数を直接呼ぶことは禁止．
-    // プリフェッチしたければSetPrefetchメソッドを利用する．
-    function Prefetch(self) {
-        for (let key in this.lastFetchedId) {
-            let start = this.lastFetchedId[key] + 1;
-            let end = start + this.prefetchNum;
-            for (let id = start; id < end; id++) {
-                let op = null;
-                try {
-                    op = self.getOp(key, id);
-                } catch(e) {
-                    console.log(e);
-                    break;
-                }
-                if (op == null) {
-                    break;
-                }
-            }
-        }
-        SetPrefetch(self);
-    }
-    */
 }
 
 module.exports.Konata = Konata;
