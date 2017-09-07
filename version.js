5ef1f40e9dec74a9a20310ca3fc362f852c1992f
Ryota Shioya <shioya@nuee.nagoya-u.ac.jp>
2017-09-07 17:51:19 +0900

    Update a version number

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 const KONATA_VERSION_INFO = {
-    about: "Konata ver 0.18, Kojiro Izuoka and Ryota Shioya."
+    about: "Konata ver 0.19, Kojiro Izuoka and Ryota Shioya."
 };
 
 module.exports.KONATA_VERSION_INFO = KONATA_VERSION_INFO;
