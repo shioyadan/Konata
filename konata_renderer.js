1aacab9064c1f92bd2fcc937af55816da4290226
Ryota Shioya <shioya@ci.i.u-tokyo.ac.jp>
2020-03-18 22:37:58 +0900

    feat: Label pane fonts are no longer expanded

--- konata_renderer.js
+++ konata_renderer.js
@@ -495,9 +495,9 @@ class KonataRenderer{
         let fontFamily = self.style_["fontFamily"];
         let fontStyle = self.style_["fontStyle"];
         let fontSize = parseInt(self.style_["fontSize"]);
-        self.labelFont_ = `${fontStyle} ${fontSize*zoomScale}px ${fontFamily}`;
+        self.labelFont_ = `${fontStyle} ${fontSize*Math.min(1.0, zoomScale)}px ${fontFamily}`;
         self.stageFont_ = `${fontStyle} ${fontSize*zoomScale}px ${fontFamily}`;
-        self.labelFontSize_ = fontSize * zoomScale;
+        self.labelFontSize_ = fontSize * Math.min(1.0, zoomScale);
         self.stageFontSize_ = fontSize * zoomScale;
     }
 
