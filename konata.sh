9fff8a0ac80b0daf26b5e657461eae025c70e236
Ryota Shioya <shioya@ci.i.u-tokyo.ac.jp>
2020-10-10 23:05:03 +0900

    fix: konata.sh now can accept input files corectly

--- konata.sh
+++ konata.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-if [ -e $(dirname $0)/node_modules ]; then
-    npx electron $(dirname $0) $1
+if [ -e $(dirname $0)/node_modules/electron/dist/electron ]; then
+    # It directly uses an Electron binary instead npx because 
+    # the current directory must not be changed so that $1 works.  
+    $(dirname $0)/node_modules/electron/dist/electron $(dirname $0) $1
 else
     echo "'node_modules' does not exit. Please install node.js and type 'node install' in this directory."-
 fi 
