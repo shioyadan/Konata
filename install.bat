b4a92e95e3f2d80eb8ea11e452d551e4d697de52
Ryota Shioya <shioya@ci.i.u-tokyo.ac.jp>
2020-12-22 21:47:38 +0900

    feat: improve the Windows install script

--- install.bat
+++ install.bat
@@ -1,6 +1,10 @@
-npm install
+cmd /c npm install
 
 @if %errorlevel% neq 0 (
     @echo Installation failed. If you have not installed node.js, please install node.js from "https://nodejs.org"
     exit 1
 )
+
+@echo Installation successful. 
+@echo Please launch Konata from "konata.vbs".
+pause
