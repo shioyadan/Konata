74cc37943975d7218baebede27b6028e082d1753
Ryota Shioya <shioya@ci.i.u-tokyo.ac.jp>
2021-10-07 19:37:27 +0900

    fix: fix a method to launch Konata from konata.konata.sh

--- konata.sh
+++ konata.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-if [ -e $(dirname $0)/node_modules/electron/dist/electron ]; then
-    # It directly uses an Electron binary instead npx because 
-    # the current directory must not be changed so that $1 works.  
-    $(dirname $0)/node_modules/electron/dist/electron $(dirname $0) $1
+if [ -e $(dirname $0)/node_modules ]; then
+    npx --prefix $(dirname $0) electron $(dirname $0) $1
 else
     echo "'node_modules' does not exit. Please install node.js and type 'node install' in this directory."-
 fi 
